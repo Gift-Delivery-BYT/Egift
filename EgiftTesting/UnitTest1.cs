804fc2d9f1a95fccc81f99ffbe22d1951040dd8f
Dauirzhan2002 <104432303+Dauirzhan2002@users.noreply.github.com>
2024-11-06 23:59:38 +0100

    UnitTests

--- EgiftTesting/UnitTest1.cs
+++ EgiftTesting/UnitTest1.cs
@@ -2,6 +2,7 @@ using NUnit.Framework;
 using Egift_main;
 using Egift_main.Order;
 using System;
+using Egift_main.Models.Order;
 
 namespace EgiftTesting
 {
@@ -11,7 +12,7 @@ namespace EgiftTesting
         private User _user;
         private Wallet _wallet;
         private Refund _refund;
-
+        private Schedule _schedule;
         [SetUp]
         public void Setup()
         {
@@ -20,6 +21,7 @@ namespace EgiftTesting
             _employee = new Employee(2, "0987654321", "employee@example.com", _wallet, "9-5", "Garry");
             _refund = new Refund();
             _employee.Refund = _refund;
+            _schedule = new Schedule();
         }
 
         [Test]
@@ -33,7 +35,7 @@ namespace EgiftTesting
         }
 
         [Test]
-        public void AddMoney_ShouldIncreaseWalletAmount()
+        public void AddMoney_IncreasesWalletAmount()
         {
             
             double initialAmount = 50.0;
@@ -60,5 +62,55 @@ namespace EgiftTesting
             Assert.IsTrue(order.IsTreckerAssigned()); // Assuming there's an IsTreckerAssigned method
         }
         //
+        
+        [Test]
+        public void Email1_ThrowException_WhenSetToNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => _user.Email1 = null);
+        }
+        [Test]
+        public void UserWallet_ShouldThrowException_WhenSetToNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => _user.UserWallet = null);
+        }
+        [Test]
+        public void Wallet_InitialBalance_ShouldBeZero()
+        {
+            var newWallet = new Wallet();
+            Assert.AreEqual(0.0, newWallet.GetAmount());
+        }
+        
+        [Test]
+        public void AddWorkHours_WorkHourAlreadyExists()
+        {
+            DateTime workDate = new DateTime(2024, 12, 1);
+            
+            _schedule.AddWorkHours(workDate);
+            _schedule.AddWorkHours(workDate);
+    
+            Assert.AreEqual(1, _schedule.ScheduleDate.Count);
+        }
+        
+        [Test]
+        public void AddHolidays_HolidayIsAdded()
+        {
+            DateTime holiday = new DateTime(2024, 12, 25);
+    
+            _schedule.AddHolidays(holiday);
+    
+            Assert.Contains(holiday, _schedule.Holidays);
+        }
+        
+        [Test]
+        public void AddHolidays_HolidayDoesntDuplicate()
+        {
+            DateTime holiday = new DateTime(2024, 12, 25);
+    
+            _schedule.AddHolidays(holiday);
+            _schedule.AddHolidays(holiday);
+    
+            Assert.AreEqual(1, _schedule.Holidays.Count);
+        }
+
     }
 }
