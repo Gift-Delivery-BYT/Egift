cb092b8d6874f9068a1904150ddb4645dd06c0a2
mkdir28 <111189785+mkdir28@users.noreply.github.com>
2024-11-10 09:45:09 +0100

    Added methods to class Item

--- EgiftTesting/obj/Debug/net8.0/EgiftTesting.AssemblyInfo.cs
+++ EgiftTesting/obj/Debug/net8.0/EgiftTesting.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EgiftTesting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e65a1c189e8b1dac02374277537d49396d66a4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d28de2347ea541df0eac6f57d98f7dc3845b804a")]
 [assembly: System.Reflection.AssemblyProductAttribute("EgiftTesting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EgiftTesting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
