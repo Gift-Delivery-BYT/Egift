4deccdc25b62aa43c4dd724e7835f7879be51dda
Maksym <maksymtautkevychius@gmail.com>
2024-12-24 13:22:20 +0100

    Merge commit

--- EgiftTesting/obj/Debug/net8.0/EgiftTesting.AssemblyInfo.cs
+++ EgiftTesting/obj/Debug/net8.0/EgiftTesting.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EgiftTesting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7ccc07e964d976da4efcccde5e8c1c5a5fd9626")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01d1d4e6f4d023286c7e3e03a0d81332e31980e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("EgiftTesting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EgiftTesting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
